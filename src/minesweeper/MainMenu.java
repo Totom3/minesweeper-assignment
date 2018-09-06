b807de7ffca69ae979b9a24c186991ea5ceae742
Cicillo <frankiebalassone@gmail.com>
2018-09-06 16:03:01 -0400

    Finished the Main Menu window (style, and function). Check if it's WAD.

--- src/minesweeper/MainMenu.java
+++ src/minesweeper/MainMenu.java
@@ -5,6 +5,10 @@
  */
 package minesweeper;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -17,31 +21,72 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
 
 /**
  *
  * @author Frankie
  */
-public class MainMenu extends JFrame implements IMainMenu {
+public class MainMenu extends JFrame implements IMainMenu {   
+    
+    private final Font defaultFont = new Font("Courier New", Font.ITALIC, 24);
+    private final Font titleFont = new Font("Courier New", Font.BOLD, 48);
+    private final JLabel gamesWon; //will display how many games are won
+    private final JLabel gamesLost;
+    
+    private final JLabel MINESWEEPER_TITLE;
+    
+    protected int winCounter = 0;
+    protected int loseCounter = 0;
+    
+    
+    private JLabel gameInProgress; //display if a game is in progress or not.
+    private final JButton startGame;
 
-    int height;
-    int width;
-
-    JLabel gamesWon = new JLabel(); //will display how many games are won
-    JLabel gamesLost = new JLabel();
-
-    JLabel gameInProgress = new JLabel(); //display if a game is in progress or not.
-    JButton startGame = new JButton("Start Game");
-
-    MainMenu(int width, int height) {
-        setSize(width, height);
+    public MainMenu(int width, int height) {
+        //NEED TO ADD A TITLE FOR THE MAIN MENU WINDOW
+        MINESWEEPER_TITLE = new JLabel("MINESWEEPER");
+        MINESWEEPER_TITLE.setFont(titleFont);
+        MINESWEEPER_TITLE.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        gameInProgress = new JLabel("^ Start a new game ^");
+        gameInProgress.setFont(defaultFont);
+        gameInProgress.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        gamesWon = new JLabel("Games won: " + winCounter); //will display the player's win/loss record (winCounter, and loseCounter respectively)
+        gamesWon.setHorizontalAlignment(SwingConstants.CENTER); //centers the text
+        gamesWon.setFont(defaultFont);
+        //gamesWon.setBorder(new LineBorder(Color.blue));
+        
+        gamesLost = new JLabel("Games lost: " + loseCounter);
+        gamesLost.setHorizontalAlignment(SwingConstants.CENTER);
+        gamesLost.setFont(defaultFont);
+        
+        
+        setSize(width, height); //sets size of the main menu window
+        
+        startGame = new JButton("Start Game");
+        //when gameInProgress = true, display: "Game in Progress", AND disable the start button
+        //when it's false, display "Start a new game"
+        
         startGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                GameBoard newGame = new GameBoard(1000,1000);
-                //create new game
+                GameBoard newGame = new GameBoard(MainMenu.this);
+                startGame.setEnabled(false); //disables Start Game button until game is ended (check onGameEnd method)
+                gameInProgress.setText("Game is in progress!");
+                gameInProgress.setHorizontalAlignment(SwingConstants.CENTER);                
             }
         });
+        
+        Container mainMenu = getContentPane();
+        mainMenu.setLayout(new GridLayout(5, 1));
+        mainMenu.add(MINESWEEPER_TITLE);
+        mainMenu.add(startGame);
+        mainMenu.add(gamesWon);
+        mainMenu.add(gamesLost);
+        mainMenu.add(gameInProgress);
     }
 
     public static void main(String[] args) {
@@ -57,14 +102,27 @@ public class MainMenu extends JFrame implements IMainMenu {
      */
     @Override
     public void onGameEnd(boolean winOrLose) {
+        
+        startGame.setEnabled(true); //re-enables the "start game" button when the game ends
+        
+        if(winOrLose) {
+            winCounter++;
+        }
+        
+        else {
+            loseCounter++; 
+        }
+        
         try {
-            File winSound = new File(winOrLose ? "Win.wav" : "Lose.wav"); //display victory/loss sound
+            File winSound = new File(winOrLose ? "Win.wav" : "Lose.wav"); //plays victory/defeat sound depending on boolean value of winOrLose
             AudioInputStream aisWin = AudioSystem.getAudioInputStream(winSound);
             Clip winClip = AudioSystem.getClip();
             winClip.open(aisWin);
             winClip.start();
-        } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
-            System.out.println("An error has occured, make sure that this file is in the .wav format, and that exists on your PC.");
+        } 
+        
+        catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
+            System.out.println("An error has occured, make sure that this file is in the .wav format, and that the file actually exists on your PC.");
         }
     }
 
